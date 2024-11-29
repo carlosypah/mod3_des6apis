9357944a59a8a73a9a0adf318b71917c6ef8a305
Carlos Munoz <carlos.munoz.delabarra@gmail.com>
2024-11-28 23:58:03 -0300

    funcionalidad de conversión, faltan validaciones y la grafica

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,58 @@
+
+const cambios = ["uf","dolar","dolar_intercambio","euro","utm"]
+const listaMonedas = document.querySelector(".moneda")
+const espacioResultado = document.querySelector(".resultado")
+
+const apiURL = "https://mindicador.cl/api"
+
+//obtener opciones y llenar el select
+//traer 4 conversiones 
+//usar try catch
+async function obtenerMonedas(){
+    try{
+        const result = await fetch(apiURL);
+        const data = await result.json();
+        var mon = [];
+        cambios.forEach((element) => {
+            mon.push(data[element])
+        });
+
+        let innerHTML = ''
+        mon.forEach((element) => {
+            innerHTML += `<option value='{"moneda":["${element.codigo}","${element.nombre}",${element.valor}]}'>${element.nombre}</option>`            
+        });
+        listaMonedas.innerHTML = innerHTML;
+        
+    }
+    catch(e){
+        //cargar lista desde json miindicador.json
+        console.log("Error al obtener la lista de monedas")
+    } 
+}
+//calcular el cambio al seleccionar y actualizar el DOM
+//tambien actualizar la gráfica
+//usar try catch
+async function convertir(){
+    try{
+        //recuperar pesos ingresados
+        let pesos = document.querySelector(".pesos");
+        //recuperar moneda seleccionada
+        let seleccion = JSON.parse(listaMonedas.value)
+        //TODO:validar si el campo Pesos no está vacío  o no hay selección
+
+        espacioResultado.innerHTML ='$'.concat((Number(pesos.value) / seleccion.moneda[2]).toFixed(2))
+
+        
+    }
+    catch(e){
+        //convertir desde el json miindicador.json
+        console.log('Error al buscar la conversión')
+    }
+}
+
+//llamada inicial
+obtenerMonedas()
+//********************************************************* */
 // //fijar varaibles
 // const usuarios = document.querySelector(".usuarios")
 // const apiURL = "https://jsonplaceholder.typicode.com/users"
